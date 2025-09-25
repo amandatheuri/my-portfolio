c69eb11675b7546a983c35fab8496a506d46e0a1
amandatheuri <amandatheuri19@gmail.com>
2025-09-25 17:28:57 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Auto-switch screenshots in hero
+const slides = document.querySelectorAll('.slide');
+let i = 0;
+setInterval(()=>{
+  slides[i].classList.remove('active');
+  i = (i+1) % slides.length;
+  slides[i].classList.add('active');
+}, 3000);
